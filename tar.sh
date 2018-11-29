2f90bfb69ff4808e1f315358f07eef6083bc07ed
Brian Blosser <bpb54321@gmail.com>
2018-11-29 14:04:48 -0500

    Added more details for tarball extraction.

--- tar.sh
+++ tar.sh
@@ -2,6 +2,9 @@
 # Compresses a folder into a gzip
 tar pczvf /var/www/tmp/assessment.georgetown.edu.tar.gz assessment.georgetown.edu
 
-# Unzip
-# tar flags name of file to unzip in place
-tar xvfz blogs.core.tar.gz
+# Unzip in place
+mkdir new_folder
+cd new_folder
+
+# Unzip the archive into the new folder
+tar xzvf ../blogs.core.tar.gz
